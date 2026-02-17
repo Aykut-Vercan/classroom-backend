0abf9f202026e5c62819d61e7a2b38a58f6b95f0
Aykut Vercan <aykutvercan@hotmail.com>
2026-02-17 19:19:30 +0300

    impl get all subjects route

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
 import express from 'express';
-import 'dotenv/config';  
+import 'dotenv/config';
+import cors from 'cors';
+
+import subjectsRouter from './routes/subjects';
 
 const app = express();
-
 const PORT: number = parseInt(process.env.PORT || '8000');
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}))
+
 
 app.use(express.json());
 
+app.use('/api/subjects', subjectsRouter)
+
+
 app.get('/', (req, res): void => {
-  res.json({ 
-    message: 'Merhaba! Sunucu çalışıyor.' 
+  res.json({
+    message: 'Merhaba! Sunucu çalışıyor.'
   });
 });
 
